adbea1c553fd2f6d29450ad4e14d34b6b5ce1c39
JZGOATED <mikeluo1027@gmail.com>
2024-03-31 16:58:31 -0400

    update

--- Q10.rkt
+++ Q10.rkt
@@ -3,8 +3,8 @@
 ;; Group work done by Junzhang Luo, Elyn Huang
 
 (define tmps-ht (make-hash))
-(define var-ht (make-hash))
-(define args-ht (make-hash))
+;;(define var-ht (make-hash))
+(define vargs-ht (make-hash))
 (define fun-name-ht (make-hash))
 (define program empty)
 
@@ -49,9 +49,11 @@
 (define (mod-sym sym)
   (string->symbol (string-append "_" (symbol->string sym))))
 
-;;TODO: write a mod-arg
-;;TODO: mod-var
-
+;; add function name in front
+;; CHECKED TODO: write a mod-arg 
+;; CHECKED TODO: mod-var
+(define (add-fun-name fn sym)
+  (string->symbol (string-append (symbol->string fn) (symbol->string sym))))
 
 ;(define (print-ht ht)
 ;  (for ([(key val) (in-hash ht)])
@@ -94,25 +96,34 @@
   (cond [(number? aexp)
          (list aexp tmpcount aexp)]
         [(symbol? aexp)
-         (define nid (mod-sym aexp))
+         (define res (hash-ref aexp))
+         (define nid (car res))
          (list aexp tmpcount nid)]
-        [(and (list? aexp) (hash-has-key? fun-name-ht (first aexp))) 
-        ;TODO: need to have the function in hash first
+        ;; Can we assume the function is pre-defined
+        [(hash-has-key? fun-name-ht (first aexp))
+        ; TODO: need to have the function in hash first
         ;;TODO: check there are the right number of parameters being passed in to the function
+        (define cur_fun (hash-ref (first aexp)))
+        (define arg-num (cadr cur_fun))
         (define params (rest aexp))
-        (define tmp-count 0) ;; Asume SP points to the end of function
-        (for [(param params)]
-          ;; Create tmp variables at the end of current stack, relative to SP
-          ;; For passing arguments to the called function
-          ;; TODO: make sure parameters are in right order
-          (set! program (cons '(move (tmp-count SP) param)))
-          (set! tmp-count (add tmp-count 1)))
-        ;; TODO: not sure if moving FP and SP is needed
-        ;;(set! SP (add SP tmp-count))
-        (set! program (cons '(jsr RETURN-ADDR (first aexp)) program))
+        (define params-len (length params))
+        (cond [(= arg-num params-len) 
+               (define tmp-count 0) ;; Asume SP points to the end of function
+               (for [(param params)]
+                 ;; Create tmp variables at the end of current stack, relative to SP
+                 ;; For passing arguments to the called function
+                 ;; TODO: make sure parameters are in right order
+                 (define 
+                 (set! program (cons (list ('move (tmp-count 'SP) param)))
+                 (set! tmp-count (add tmp-count 1)))
+               ;; TODO: not sure if moving FP and SP is needed
+               ;;(set! SP (add SP tmp-count))
+               (set! program (cons '(jsr RETURN-ADDR (first aexp)) program))]
         ;;(set! SP (sub SP tmp-count))
-        ]
-        [else
+              [else (error "incorrect number of arguments")])
+        
+        
+        [(hash-has-key? aexpop-ht (first aexp))
          (match aexp
            [`(,op ,aexp1 ,aexp2)
             (define nop (hash-ref aexpop-ht op))
@@ -122,11 +133,11 @@
             (define res2 (eval-aexp tmpcount aexp2))
             (define var2 (car res2))
             (set! tmpcount (+ (cadr res2) tmpcount))
-            (cond [(and (number? var1) (hash-has-key? var-ht var2))
+            (cond [(and (number? var1) (hash-has-key? vargs-ht var2))
                    (define res (list nop (caddr res2) (caddr res1) (caddr res2)))
                    (set! program (cons res program))
                    (list #\0 tmpcount (caddr res2))]
-                  [(and (number? var2) (hash-has-key? var-ht var1))
+                  [(and (number? var2) (hash-has-key? vargs-ht var1))
                    (define res (list nop (caddr res1) (caddr res1) (caddr res2)))
                    (set! program (cons res program))
                    (list #\0 tmpcount (caddr res1))]
@@ -135,7 +146,8 @@
                    (set! tmpcount (+ tmpcount 1))
                    (define res (list nop ntmp (caddr res1) (caddr res2)))
                    (set! program (cons res program))
-                   (list ntmp tmpcount ntmp)])])]))
+                   (list ntmp tmpcount ntmp)])])]
+        [else (error "undefined function")]))
 
 ;; eval-bexp: tmpcount bexp
 ;; - update program and tmpcount
@@ -193,10 +205,13 @@
 ;; interp a stmt
 (define (interp loop stmt tmpcount)
   (match stmt
-    [`(return ,x) 
+    [`(return ,x)
     ;;TODO: after x is evaluated, x needs to be stored in the global RETURN-VAL
-    (eval-aexp 0 x) ;; TODO: assume return is the last statement to call
-    ]
+    (define res (eval-aexp tmpcount x)) ;; TODO: assume return is the last statement to call
+    (set! tmpcount (+ cadr res) tmpcount)
+    ;; the previous line will return the name to move FP to
+    (set! program (cons (list 'move 'RETURN-VAL (list ____ 'FP)) program))
+    (if loop tmpcount (set! s-cur (- s-cur tmpcount)))]
     [`(print ,s)
      (cond [(string? s)
             (define nline (list 'print-string s))
@@ -285,17 +300,24 @@
   (for ([(key value) (in-hash tmps-ht)])
     (define res (list 'data key value))
     (set! program (cons res program))))
-    
+
+
+;; what about empty function
 (define (eval-stmts stmts)
-  (cond [(empty? stmts)
-         (define nline (list 'halt))
-         (set! program (cons nline program))
-         (for ([(key value) (in-hash var-ht)])
-           (define mvar (mod-sym key))
-           (define res (cons (list 'data mvar value) program))
-           (set! program res))
-         (insert-data)
-         (reverse program)]
+  (cond [(empty? stmts) (error "no statements in function")]
+        [(empty? (rest stmts))
+         (cond [(symbol=? (car (car stmts)) 'return)
+                (interp #f (car stmts) 0)]
+               [else (error "last statement is not return")])]
+        
+;         (define nline (list 'halt))
+;         (set! program (cons nline program))
+;         (for ([(key value) (in-hash var-ht)])
+;           (define mvar (mod-sym key))
+;           (define res (cons (list 'data mvar value) program))
+;           (set! program res))
+;         
+;         (reverse program)]
         [else (interp #f (car stmts) 0) (eval-stmts (cdr stmts))]))
 
 ;; Assume the sexp is a list
@@ -307,68 +329,151 @@
 ;; local variables
 ;; RETURN-ADDR
 ;; RETURN-VAL
-(define (compile-function sexp)
-  (define local-fp-name (mod-arg fun-name "FP"))
-  (define local-return-addr (mod-arg fun-name "RETURN-ADDR"))
-    (hash-set! var-ht (make-hash))
-    (hash-set! tmps-ht (make-hash)) ;; tmps is 
-    (define args (rest (second sexp)))
-    (define fun-name (mod-sym (first (second sexp))))
-    ;; TODO: check for main existence
-  (define vars (second (third sexp)))
-  (define stmts (third (third sexp)))
-  (define local-size (mod-sym fun-name "SIZE"))
-  (cons '(label fun-name) program)
-  (if (hash-has-key? fun-name-ht fun-name)
-  (error "duplicate")
-  (hash-set! fun-name-ht fun-name 0)) ;;TODO: return void
-  (define counter -1)
+(define (compile-function fun)
+  (set! vargs-ht (make-hash))
+  ;; (set! tmps-ht (make-hash))
+  (define fun-name (first (second fun)))
+  (define nfun-name (mod-sym fun-name))
+  (set! program (cons (list 'label nfun-name) program))
+  ;; Args
+  (define args (rest (second fun)))
+  (define args-len (length args))
+  (define arg-counter 0)
+  ;; (hash-set! fun-name-ht fun-name (list nfun-name args-len))
   (for ([arg args])
-    (define marg (mod-arg fun-name arg))
-    (define arg-line '(const marg counter))
-    (if 
-    (hash-has-key? args-ht arg)
-    (error "duplicate")
-    (hash-set! args-ht arg arg-line))
-    (set! program (cons arg-line program))
-    (set! counter (- counter 1)))
-  (set! program (cons '(const local-fp-name 0) program))
-  (set! program (cons '(const local-return-addr 1) program))
-  (set! counter 2)
+    (if (hash-has-key? vargs-ht arg) (error "duplicate")
+        (set! arg-counter (- arg-counter 1))
+        (define narg (add-fun-name nfun-name arg))
+        (hash-set! vargs-ht arg (list narg arg-counter))
+        (define arg-line (list 'const narg arg-counter))
+        (set! program (cons arg-line program))))
+  ;; FP
+  (define nFP (add-fun-name nfun-name 'FP))
+  (set! program (cons (list 'const nFP 0) program))
+  ;; RTA
+  (define nRTA (add-fun-name nfun-name 'RETURN-ADDR))
+  (set! program (cons (list 'const nRTA 1) program))
+  ;; Local vars
+  (define vars (second (third fun)))
+  (define vars-len (length vars))
   (for ([var vars])
-    (define mvar (mod-sym fun-name (car var)))
-    (define val (cadr var))
-    (if 
-      (hash-has-key? var-ht var) 
-      (error "duplicate") ;;TODO: will return void if error
-      (hash-set! var-ht var val))
-    (set! program (cons '(data mvar counter)))
-    (set! counter (add counter 1)))
-  (set! program (cons local-size (+ 1 (length vars))))
+    (define var-name (car var))
+    (if (hash-has-key? vargs-ht var-name) (error "duplicate")
+        (define nvar-name (add-fun-name nfun-name var-name))
+        (define value (cadr var))
+        (hash-set! vargs-ht var-name (list nvar-name value)) ;; (list new_name init_val)
+        (set! program (cons (list 'const nvar-name value) program))))
+  ;; Stack frame size
+  (define size (+ vars-len 2))
+  (define size-name (add-fun-name nfun-name 'SIZE))
+  (set! program (cons (list 'const size-name size) program))
   ;; Prologue
-  (set! program (cons '(move (local-fp-name SP) FP) program))
-  (set! program (cons '(move (local-return-addr SP) RETURN-ADDR) program))
-  (for [(var (in-hash var-ht))]
-    (set! program (cons '(move ((mod-sym fun-name var) SP) val))))
-  (set! program (cons '(move FP SP) program))
-  (set! program (cons '(add SP SP local-size)))
+  (set! program (cons (list 'move (list nFP 'SP) 'FP) program))
+  (set! program (cons (list 'move (list nRTA 'SP) 'RETURN-ADDR) program))
+  ;; Prologue loc_vars
+  (for ([var vars])
+    (define var-name (car var))
+    (define res (hash-ref vargs-ht var-name))
+    (define nvar-name (car res))
+    (define val (cadr res))
+    (set! program (cons (list 'move (list nvar-name 'SP) val) program)))
+  (set! program (cons (list 'move 'FP 'SP) program))
+  (set! program (cons (list 'add 'SP 'SP size-name) program))
   ;; Body
-  (eval-stmts stmts)
+  (define body (cddr (third fun)))
+  (eval-stmts body)
   ;; Epilogue
-  (set! program (cons '(sub SP SP local-size) program))
-  (set! program (cons '(move FP (local-fp-name SP)) program))
-  (set! program (cons '(move RETURN-ADDR (local-return-addr FP))))
-  (set! program (cons '(jump RETURN-ADDR))))
+  (set! program (cons (list 'sub 'SP 'SP size-name) program))
+  (set! program (cons (list 'move 'FP (list nFP 'SP)) program))
+  (set! program (cons (list 'move 'RETURN-ADDR (list nRTA 'SP)) program))
+  (set! program (cons (list 'jump 'RETURN-ADDR) program)))
+         
 
-(define (compile-program losexp)
-    (set! program empty)
-    (for [(func losexp)]
-      (compile-function func))
+
+
+         
+;         (define local-fp-name (mod-arg fun-name "FP"))
+;         (define local-return-addr (mod-arg fun-name "RETURN-ADDR"))
+;         (hash-set! var-ht (make-hash))
+;         (hash-set! tmps-ht (make-hash)) ;; tmps is 
+;         (define args (rest (second sexp)))
+;    
+;         ;; TODO: check for main existence
+;         (define vars (second (third sexp)))
+;         (define stmts (third (third sexp)))
+;         (define local-size (mod-sym fun-name "SIZE"))
+;         (cons '(label fun-name) program)
+;         (if (hash-has-key? fun-name-ht fun-name)
+;             (error "duplicate")
+;             (hash-set! fun-name-ht fun-name 0)) ;;TODO: return void
+;         (define counter -1)
+;         (for ([arg args])
+;           (define marg (mod-arg fun-name arg))
+;           (define arg-line '(const marg counter))
+;           (if 
+;            (hash-has-key? args-ht arg)
+;            (error "duplicate")
+;            (hash-set! args-ht arg arg-line))
+;           (set! program (cons arg-line program))
+;           (set! counter (- counter 1)))
+;         (set! program (cons '(const local-fp-name 0) program))
+;         (set! program (cons '(const local-return-addr 1) program))
+;         (set! counter 2)
+;         (for ([var vars])
+;           (define mvar (mod-sym fun-name (car var)))
+;           (define val (cadr var))
+;           (if 
+;            (hash-has-key? var-ht var) 
+;            (error "duplicate") ;;TODO: will return void if error
+;            (hash-set! var-ht var val))
+;           (set! program (cons '(data mvar counter)))
+;           (set! counter (add counter 1)))
+;         (set! program (cons local-size (+ 1 (length vars))))
+;         ;; Prologue
+;         (set! program (cons '(move (local-fp-name SP) FP) program))
+;         (set! program (cons '(move (local-return-addr SP) RETURN-ADDR) program))
+;         (for [(var (in-hash var-ht))]
+;           (set! program (cons '(move ((mod-sym fun-name var) SP) val))))
+;         (set! program (cons '(move FP SP) program))
+;         (set! program (cons '(add SP SP local-size)))
+;         ;; Body
+;         (eval-stmts stmts)
+;         ;; Epilogue
+;         (set! program (cons '(sub SP SP local-size) program))
+;         (set! program (cons '(move FP (local-fp-name SP)) program))
+;         (set! program (cons '(move RETURN-ADDR (local-return-addr FP))))
+;         (set! program (cons '(jump RETURN-ADDR)))]))
+
+(define (compile-program lofun)
+  (set! program empty)
+  (cond [(empty? lofun)
+         (insert-data) ;; insert all tmp variables initialized
+         (set! program (cons '(data RETURN-VAL 0) program))
+         (set! program (cons '(data RETURN-ADDR 0) program))
+         (set! program (cons '(data FP 0) program))
+         (set! program (cons '(data SP END) program))
+         (set! program (cons '(label END) program))
+         (cond [(hash-has-key? fun-name-ht 'main) ;; check for main existence
+                (set! program (cons '(jump main) program))
+                (reverse program)]
+               [else
+                (set! program (cons '(halt) program))
+                (reverse program)])]
+        [else (compile-function (first lofun)) (compile-program (rest lofun))]))
+
+(define (compile-simpl lofun)
+  (for ([fun lofun])
+    (define f-name (car (cadr fun)))
+    (define arg-len (cdr (cadr fun)))
+    (cond [(hash-has-key? fun-name-ht f-name) (error "duplicate")]
+          [else (define nf-name (mod-sym f-name))
+                (hash-set! fun-name-ht f-name (list nf-name arg-len))]))
+  (compile-program lofun))
+
+;    (for [(func losexp)]
+;      (compile-function func))
     ;; Global datas
-    (set! program (cons '(data RETURN-VAL 0) program))
-    (set! program (cons '(data RETURN-ADDR 0) program))
-    (set! program (cons '(data FP 0) program))
-    (set! program (cons '(data SP" 0) program)))
+    
 
 ;(define test
 ;  '(var ((x 10) (y 1))
